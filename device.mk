85538ce4d5219e21d0b7e7d62b1670ca146c2b55
Arian <arian.kulmer@web.de>
2023-07-29 15:53:16 +0545

    olive : Import sensor configs from olive 10 QKQ1.191014.001 V12.5.3.0.Q  and Import fingerprint service * from https://android.googlesource.com/platform/hardware/interfaces/+/744b02880730ffcb1cf4dade6269e81819a4a5ba
    Signed-off-by: ChresthaManish <Shresthamanish6067@gmail.com>

--- device.mk
+++ device.mk
@@ -16,9 +16,22 @@ $(call inherit-product, device/xiaomi/sdm439-common/sdm439.mk)
 TARGET_SCREEN_HEIGHT := 1440
 TARGET_SCREEN_WIDTH := 720
 
+# Sensors
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
+    
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml    
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
+    
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_olive \
+    android.hardware.biometrics.fingerprint@2.2
 
 # Inherit from vendor blobs
 $(call inherit-product, vendor/xiaomi/olive/olive-vendor.mk)
